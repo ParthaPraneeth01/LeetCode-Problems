f7a1cb9152fa45993e2117669cc8305a98e01a29
ParthaPraneeth01 <parthapraneeth01@gmail.com>
2025-06-04 00:50:02 +0530

    Update leetCode155_MinStack.java

--- leetCode155_MinStack.java
+++ leetCode155_MinStack.java
@@ -37,35 +37,3 @@ class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
- public class leetCode155_MinStack {
-    public static void main(String[] args) {
-        // Example Input
-        String[] operations = {"MinStack", "push", "push", "push", "getMin", "pop", "top", "getMin"};
-        int[][] values =      {{},       {-2},   {0},    {-3},     {},      {},   {},     {}};
-
-        MinStack obj = null;
-
-        for (int i = 0; i < operations.length; i++) {
-            switch (operations[i]) {
-                case "MinStack":
-                    obj = new MinStack();
-                    System.out.println("null");
-                    break;
-                case "push":
-                    obj.push(values[i][0]);
-                    System.out.println("null");
-                    break;
-                case "pop":
-                    obj.pop();
-                    System.out.println("null");
-                    break;
-                case "top":
-                    System.out.println(obj.top());
-                    break;
-                case "getMin":
-                    System.out.println(obj.getMin());
-                    break;
-            }
-        }
-    }
-}
